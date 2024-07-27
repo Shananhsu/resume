8c650be3168f9450878027c3133b8b6a27a7daf6
shananhsu <shananhsu@gmail.com>
2024-07-27 13:23:44 +0800

    新增 TailwindCss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
+//components
+import Home from './home'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Home />
+	</React.StrictMode>,
 )
