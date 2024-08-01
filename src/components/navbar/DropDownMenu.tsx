55c32d7d86871accd26d18cb0063273d847dda77
shananhsu <shananhsu@gmail.com>
2024-08-01 16:28:46 +0800

    新增 1.使用react-router-dom工具提示使用者目前停留的頁面

--- src/components/navbar/DropDownMenu.tsx
+++ src/components/navbar/DropDownMenu.tsx
@@ -1,5 +1,5 @@
 //react router dom
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 //導覽列資料
 import { menuData } from "./data";
@@ -14,10 +14,15 @@ const DropDownMenu = ({ handleToggle }: handleToggleType) => {
     <div className="fixed w-full h-screen bg-mask bg-opacity-50 left-0 top-0 flex items-start">
       <div className="w-10/12 bg-black mx-auto mt-12">
         {menuData.map((item) => (
-          <div key={item.id}>
-            <Link to={item.link} className="block py-4 text-white text-center">
-              {item.title}
-            </Link>
+          <div key={item.id} className="w-full text-center">
+            <NavLink
+              to={item.link}
+              className={({ isActive }) =>
+                `${isActive ? "text-white" : "text-gray-400"}`
+              }
+            >
+              <p className="py-4">{item.title}</p>
+            </NavLink>
           </div>
         ))}
         <button className="w-full text-center py-4" onClick={handleToggle}>
