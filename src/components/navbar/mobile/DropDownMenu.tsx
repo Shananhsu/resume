6a07c145be6050ce7bb2f106445124f81fd8e808
shananhsu <shananhsu@gmail.com>
2024-08-08 08:53:33 +0800

    修改 1.手機版本下拉選單樣式

--- src/components/navbar/mobile/DropDownMenu.tsx
+++ src/components/navbar/mobile/DropDownMenu.tsx
@@ -1,6 +1,9 @@
 //react router dom
 import { NavLink, useNavigate } from "react-router-dom";
 
+//react icons
+import { IoLocationOutline } from "react-icons/io5";
+
 //導覽列資料
 import { menuData } from "../data";
 
@@ -22,17 +25,27 @@ const DropDownMenu = ({ handleToggle }: handleToggleType) => {
     <div className="fixed w-full h-screen bg-mask bg-opacity-50 left-0 top-0 flex items-start">
       <div className="w-10/12 bg-black mx-auto mt-12">
         {menuData.map((item) => (
-          <div key={item.id} className="w-full text-center">
-            <NavLink
-              to={item.link}
-              className={({ isActive }) =>
-                `${isActive ? "text-white" : "text-gray-400"}`
-              }
-              onClick={() => handleLinkClick(item.link)}
-            >
-              <p className="py-4">{item.title}</p>
-            </NavLink>
-          </div>
+          <NavLink
+            key={item.id}
+            to={item.link}
+            className={({ isActive }) =>
+              `${isActive ? "text-white" : "text-gray-400"}`
+            }
+            onClick={() => handleLinkClick(item.link)}
+          >
+            {({ isActive }) => (
+              <div className="py-4 flex justify-center">
+                <div className="w-1/2 flex items-center relative">
+                  {isActive && (
+                    <span className="absolute left-0">
+                      <IoLocationOutline className="text-green-500 animate-bounce" />
+                    </span>
+                  )}
+                  <p className="w-full text-center">{item.title}</p>
+                </div>
+              </div>
+            )}
+          </NavLink>
         ))}
         <button
           className="w-full text-center py-4 text-white"
